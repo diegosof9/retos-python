ebea17594372f4fc6c8e39c37942321d0d857bfc
Diego Hernández <diegosof28512@gmail.com>
2025-09-08 20:55:41 -0600

    Crea .txt

--- conversion_title.py
+++ conversion_title.py
@@ -1,9 +1,30 @@
-nombres = ['SOFíA', 'DIEGO', 'MARICeLA', 'JUAN ramón']
+nombres = ['SOFíA', 'DIEGO', 'MARICeLA', 'JUAN ramón', "teSt"]
 
 nombres_propios = []
 
+contador = 0
+
 for nombre in nombres:
 
+    # nombres_propios.append(nombre.title())
+    
+    # if contador < len(nombres)-1: 
+        
+    #     print(nombre.title(), end=', ')
+    #     contador += 1
+
+    # else:
+
+    #     print(nombre.title())
     nombres_propios.append(nombre.title())
 
+
 print(nombres_propios)
+
+with open("prueba_1.txt", "w", encoding="utf-8") as f:
+
+    f.write(", ".join(nombres_propios))
+
+# print(nombres_propios)
+
+# print(len(nombres)-1)
